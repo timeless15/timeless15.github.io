fdfa89a9cd879d241b84d945cacf01653a17b46a
timeless15 <hsq5218@163.com>
2018-05-23 23:02:07 +0800

    anni

---
+++ anni/js/data.js
@@ -0,0 +1,378 @@
+var imgData=[
+    {
+        "text": "2.1 表情包斗图 这种翻老底的感觉真的是啧啧（你看，从一开始斗图就没赢过我 辣鸡",
+        "src": "https://s26.postimg.cc/6t9pwvl1l/anni00001.jpg"
+    },
+    {
+        "text": "2.7 不要脸的蹭了一顿饭，偷偷的拍了一张图嘻嘻（然后到我家拷视频还被我妈撞了个正着。。",
+        "src": "https://s26.postimg.cc/ntsm5jind/anni00002.jpg"
+    },
+    {
+        "text": "所以下午就去了老富中回忆往昔？感觉也挺奇妙的",
+        "src": "https://s26.postimg.cc/3mf6d98bd/anni00003.jpg"
+    },
+    {
+        "text": "2.11 爱乐之城 好像不是第一次一起去看电影 但一定是印象最深的一次吧",
+        "src": "https://s26.postimg.cc/wc229vhg9/anni00004.jpg"
+    },
+    {
+        "text": "3.25-28 厚着脸皮去上海找你玩，从老校区到植物园到外滩 第一天暴走了3w步 但是印象最深的还是一直在和你聊啊聊啊聊啊聊啊",
+        "src": "https://s26.postimg.cc/xec8shd55/anni00005.jpg"
+    },
+    {
+        "text": "晚上在外滩还一不小心遇上了地球日熄灯活动",
+        "src": "https://s26.postimg.cc/e98zio115/anni00006.jpg"
+    },
+    {
+        "text": "第二天打卡了坑爹的二三十年代",
+        "src": "https://s26.postimg.cc/5qzjedcix/anni00007.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/6gibqqks9/anni00008.jpg"
+    },
+    {
+        "text": "吃了不怎么好吃的鸡蛋仔冰淇淋",
+        "src": "https://s26.postimg.cc/mrifn1huh/anni00009.jpg"
+    },
+    {
+        "text": "其实这次来憋了好多话想说呀 但还是什么都没说",
+        "src": "https://s26.postimg.cc/51gr1zwjt/anni00010.jpg"
+    },
+    {
+        "text": "4月 你说你确定去nyu了，我记得那天是我刚到图书馆，不知道怎么回你，删了又打打了又删，确实挺难受的，但又觉得说什么都不合适，然后靠着毕设强行转移自己的注意力，都挺憋的吧",
+        "src": "https://s26.postimg.cc/wc229wrqx/anni00011.jpg"
+    },
+    {
+        "text": "5月 毕设做完了，想去大西北了，其实你知不知道，当时看各种攻略的时候我想的都是和你去（虽然后来还是一起去了╮(￣▽￣)╭",
+        "src": "https://s26.postimg.cc/vmj9xjjhl/anni00012.jpg"
+    },
+    {
+        "text": "脑子一热说去迪士尼吧，你也就同意了，然后就这么去了，如果不去可能就。。没有然后了？",
+        "src": "https://s26.postimg.cc/jxfa9mi95/anni00013.jpg"
+    },
+    {
+        "text": "我们拍的第一张合照！（大妈实力抢镜 这种忘记拿掉手机上过山车又顺理成章的拍照→_→然后后面拍照就放开了哈哈哈",
+        "src": "https://s26.postimg.cc/ytdth7ly1/anni00014.jpg"
+    },
+    {
+        "text": "所以第二天在过山车上疯狂拍照？表情太精彩",
+        "src": "https://s26.postimg.cc/3mf6dbswp/anni00015.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/4opcvvjft/anni00016.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/ow2so742h/anni00017.jpg"
+    },
+    {
+        "text": "你在看什么👀",
+        "src": "https://s26.postimg.cc/c4omhoeux/anni00018.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/hg3j2e8nd/anni00019.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/jknw3i555/anni00020.jpg"
+    },
+    {
+        "text": "猪头给我拍的第一张照！从不好意思到各种使唤你拍照嘻嘻",
+        "src": "https://s26.postimg.cc/7vjwfj6gp/anni00021.jpg"
+    },
+    {
+        "text": "迪士尼真的玩得挺开心的。后面发生的一系列事就。。哼你这个一点都不走心的告白。其实现在想起来我就记着那天一开始心里好闷好闷啊，然后去全家买了一支海盐味的冰淇淋，然后回去睡了一觉，然后就感觉什么事也没有了。。是的你没有看错冰淇淋把我治愈了，所以要你有什么用🙂",
+        "src": "https://s26.postimg.cc/wc229zzhl/anni00022.jpg"
+    },
+    {
+        "text": "6.10-12你悄咪咪的来了杭州，是的那次一张照片都没有（反正我是没 你就不知道了，那两天一直在下雨下雨下雨。然后好像就打开了没羞没躁的大门哦嘿嘿嘿",
+        "src": "https://s26.postimg.cc/vmj9xo1ix/anni00023.jpg"
+    },
+    {
+        "text": "悄咪咪的换上了头像 还在猜谁会最先发现",
+        "src": "https://s26.postimg.cc/mer1gymqx/anni00024.jpg"
+    },
+    {
+        "text": "舞会。真的是做了好多准备工作，专门去买裙子（虽然最后没穿着，找搭配，毕竟还是紧张的，就突然一下要见到你的各种同学，还是想表现的好一点呀。其实现在想起来我觉得最紧张的是你吧，一个我们也刚开始不稳定，另外毕竟那啥。。对吧。反正还是要谢谢猪头 也是很难得的体验啊",
+        "src": "https://s26.postimg.cc/qnvrj4iah/anni00025.jpg"
+    },
+    {
+        "text": "舞会上的照片最喜欢的还是那段视频",
+        "src": "https://s26.postimg.cc/s2xc7ve8p/anni00026.jpg"
+    },
+    {
+        "text": "拍毕业照，真的是执念啊，但是不觉得那天运气很好吗，有了学士服，天气看着要下雨也没下下来，还有大逼哥的相机，最后拍下来虽然很累但很开心啊",
+        "src": "https://s26.postimg.cc/rdejvilex/anni00027.jpg"
+    },
+    {
+        "text": "然后就发现大哥拍照技术不错啊，感觉我们拍的真的不比专门找工作室拍的差呀（哼后期不都还是我p的",
+        "src": "https://s26.postimg.cc/b2efz6thl/anni00028.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/eyrrv7ort/anni00029.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/ojbei36e1/anni00030.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/z657ni6tl/anni00031.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/4opcvybqx/anni00032.jpg"
+    },
+    {
+        "text": "哈哈哈哈大哥的第一个表情包诞生！大概是这个时候开始玩表情包的",
+        "src": "https://s26.postimg.cc/wotgg9pi1/anni00033.jpg"
+    },
+    {
+        "text": "微博还被交大翻牌了，啧啧啧大概是唯一一下网红了。感觉我到现在没事翻照片的时候，还是喜欢看那次拍的毕业照，真好。",
+        "src": "https://s26.postimg.cc/sfoqe36t5/anni00034.jpg"
+    },
+    {
+        "text": "各种努力下的大西北之行，第一次和你出去玩就这么远。那十天大概是目前和你最开心的回忆了，出去玩整天整天呆一块，怎么都聊不完的天，天天可以和你睡觉，我怎么抱怨你都不会烦，还负责专业拍照（还有了一条点赞最多的票圈。大西北皂片太多啦，随便放几张意思一下",
+        "src": "https://s26.postimg.cc/iidpl16x5/anni00035.jpg"
+    },
+    {
+        "text": "呀大肚子了",
+        "src": "https://s26.postimg.cc/5e858dwvt/anni00036.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/51gr26c15/anni00037.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/ygmfb7bft/anni00038.jpg"
+    },
+    {
+        "text": "此处应有表情包",
+        "src": "https://s26.postimg.cc/cu7eu655l/anni00039.jpg"
+    },
+    {
+        "text": "这张还蛮喜欢的，就是镜子有点脏",
+        "src": "https://s26.postimg.cc/djq76k8a1/anni00040.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/iidpl21s9/anni00041.jpg"
+    },
+    {
+        "text": "这么妖娆 敢不敢看live？",
+        "src": "https://s26.postimg.cc/5qzjekmvd/anni00042.jpg"
+    },
+    {
+        "text": "沙漠里露营那次还是很爽的是不是！还看到了两次流星",
+        "src": "https://s26.postimg.cc/4opcw01h5/anni00043.jpg"
+    },
+    {
+        "text": "憔悴的两个人爬起来看日出",
+        "src": "https://s26.postimg.cc/vmj9xrwex/anni00044.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/4opcw2ts9/anni00045.jpg"
+    },
+    {
+        "text": "💩",
+        "src": "https://s26.postimg.cc/apn1t4t95/anni00046.jpg"
+    },
+    {
+        "text": "哼",
+        "src": "https://s26.postimg.cc/em0dp46ix/anni00047.jpg"
+    },
+    {
+        "text": "❤️",
+        "src": "https://s26.postimg.cc/b2efzaw3d/anni00048.jpg"
+    },
+    {
+        "text": "橡皮山骑马团",
+        "src": "https://s26.postimg.cc/6gibqz5fd/anni00049.jpg"
+    },
+    {
+        "text": "鞋子是咸的",
+        "src": "https://s26.postimg.cc/6gibr0809/anni00050.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/qnvrjaq21/anni00051.jpg"
+    },
+    {
+        "text": "啊真的好像再去一次青海湖 太舒服了",
+        "src": "https://s26.postimg.cc/kzpgsdquh/anni00052.jpg"
+    },
+    {
+        "text": "在淘宝上也没搜到的汽水。。",
+        "src": "https://s26.postimg.cc/b2efzc6e1/anni00053.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/7vjwfrytl/anni00054.jpg"
+    },
+    {
+        "text": "大西北",
+        "src": "https://s26.postimg.cc/tuqb2z089/anni00055.jpg"
+    },
+    {
+        "text": "大西北回来之后还是要面对现实，印象里我们聊了好几次正事吧，对未来、对异国的不安恐惧，那个时候不敢想你出国之后我会怎么样。然后还纠结怎么和爸妈说（虽然拖拖拖又是到了最后几天。",
+        "src": "https://s26.postimg.cc/6t9px7l55/anni00056.jpg"
+    },
+    {
+        "text": "8.20还是来了啊。我想你那个时候应该比我更恐惧吧，我可能只要担心我们的感情问题，你还有完全新的生活环境、学习压力，什么都要考虑，什么都只能靠你自己。记着去机场的路上一直握着手，我特别怕忘了被你握着是什么感觉。（没找到你妈这张的原图凑个数",
+        "src": "https://s26.postimg.cc/4bxypwynt/anni00057.jpg"
+    },
+    {
+        "text": "刚走的那几天真难受啊。但看你慢慢地适应新的生活，一点点把事情处理好，还是欣慰的",
+        "src": "https://s26.postimg.cc/thywwsa8p/anni00058.jpg"
+    },
+    {
+        "text": "后来也慢慢适应了吧，每天固定两个电话，周末视频。大概每天最期待的莫过于这个时候了吧，虽然有时候网不好真挺心累的",
+        "src": "https://s26.postimg.cc/u7hp94vcp/anni00059.jpg"
+    },
+    {
+        "text": "视频于是就成了black的开端",
+        "src": "https://s26.postimg.cc/i5mbez6op/anni00060.jpg"
+    },
+    {
+        "text": "从微信视频到FaceTime，感觉打开新世界。我觉得我们哪天（比如结婚？应该好好感谢微信语音和FaceTime的大力支持",
+        "src": "https://s26.postimg.cc/j7whxix7t/anni00061.jpg"
+    },
+    {
+        "text": "猪头觉得最经典的一张",
+        "src": "https://s26.postimg.cc/ntsm5wlbt/anni00062.jpg"
+    },
+    {
+        "text": "同事小姐姐送的这个真的好喜欢，每天一抬头就能看到",
+        "src": "https://s26.postimg.cc/hsux8ue55/anni00063.jpg"
+    },
+    {
+        "text": "共享相册这个东西用的真是妙啊，这个也是我们沟通很重要的一趴啊",
+        "src": "https://s26.postimg.cc/acvnn25vd/anni00064.jpg"
+    },
+    {
+        "text": "💩哥因为拉屎错过了X这个梗，承包了我好几个月的笑点",
+        "src": "https://s26.postimg.cc/nh17zqq7d/anni00065.jpg"
+    },
+    {
+        "text": "四个多月的时间，我们一点点适应各自的生活节奏，一步步探索沟通的模式。中间也吵了很多次吧，但你脾气真好啊。虽然你一直不在我身边，但我从害怕担心到越来越确定。",
+        "src": "https://s26.postimg.cc/i5mbf1jk9/anni00066.jpg"
+    },
+    {
+        "text": "12.28 终于等到了你回国 见到人的时候就感觉很熟悉又好陌生",
+        "src": "https://s26.postimg.cc/t57iqnpex/anni00067.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/v9rvrqyrd/anni00068.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/ka6og4swp/anni00069.jpg"
+    },
+        {
+        "text": "",
+        "src": "https://s26.postimg.cc/jxfa9zd7t/anni00070.jpg"
+    },
+    {
+        "text": "大型捉奸现场",
+        "src": "https://s26.postimg.cc/qnvrjeky1/anni00071.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/o6k0c58rd/anni00072.jpg"
+    },
+    {
+        "text": "想想那两周的活动密度是挺大，和我爸妈吃饭，泡温泉，外加合作愉快。真的辛苦大哥天天跑杭州。",
+        "src": "https://s26.postimg.cc/3z6kjw109/anni00073.jpg"
+    },
+    {
+        "text": "其实你刚回来那会我挺悲观的，感觉一回来又可以倒计时你要回去的日子了。但是后来元旦那天你回富阳，本来在路口就说再见了，听到你说在车站要等很久我又骑了个车吭哧吭哧赶过来了，虽然到了你已经上车了，但是隔着车窗打电话的时候，看着你，就好像突然很心安了。然后我就不会再老想着你要走了你要走了，好好珍惜在一块的时间呗。所以你走的那天我也没去送你，就是特别淡定吧。觉得你不过就是去了另外一个地方，我们该怎样还是怎样",
+        "src": "https://s26.postimg.cc/9ncvarahl/anni00074.jpg"
+    },
+    {
+        "text": "刚回去那几天你就感冒+时差，挺心疼的（看这油头",
+        "src": "https://s26.postimg.cc/g11ydzzy1/anni00075.jpg"
+    },
+    {
+        "text": "🐸蛙儿子",
+        "src": "https://s26.postimg.cc/dwhlcziwp/anni00076.jpg"
+    },
+    {
+        "text": "终于收到了明信片！但你的拖延症真。。不能忍",
+        "src": "https://s26.postimg.cc/kmy2mdgc9/anni00077.jpg"
+    },
+    {
+        "text": "你每次拉屎的时候都是这种慈母般的表情看着我",
+        "src": "https://s26.postimg.cc/lp894wc09/anni00078.jpg"
+    },
+    {
+        "text": "远程指导尝试的蛋糕",
+        "src": "https://s26.postimg.cc/nh17zu5nt/anni00079.jpg"
+    },
+    {
+        "text": "猪头和呱呱的标志性微笑 检验真伪",
+        "src": "https://s26.postimg.cc/dwhlcxlgp/anni00080.jpg"
+    },
+    {
+        "text": "其实最害怕的还是周末，特别是闲的时候。天气一好，就特别特别想和你出去玩。哎虽然这个也办法，但还是想啊",
+        "src": "https://s26.postimg.cc/iv53riufd/anni00081.jpg"
+    },
+    {
+        "text": "暗搓搓地想了好久生日送什么好呢。哎你经常说不知道送什么给我，其实我也不知道啊。反正心照不宣都表现地开心点🙂",
+        "src": "https://s26.postimg.cc/51gr2gmeh/anni00082.jpg"
+    },
+    {
+        "text": "大型爆笑现场",
+        "src": "https://s26.postimg.cc/brx8bvmex/anni00083.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/qb4dd9seh/anni00084.jpg"
+    },
+    {
+        "text": "这个学期，卖萌互怼好像成了我们的常态，说话也越来越不害臊了",
+        "src": "https://s26.postimg.cc/i5mbf56qh/anni00085.jpg"
+    },
+    {
+        "text": "哇真快啊 嗖的一下猪头又回来了。这四个月真的是很快。虽然见到的时候还被你吓了一条，但见到之后感觉很自然，身上的味道、感觉一点都没变，我们之间的那种距离感一点都没有，心里就是很踏实",
+        "src": "https://s26.postimg.cc/5e858o795/anni00086.jpg"
+    },
+    {
+        "text": "这张真的是看一次笑一次，太经典了",
+        "src": "https://s26.postimg.cc/fbj61pzfd/anni00087.jpg"
+    },
+    {
+        "text":"猪头真好啊，说一声就吭哧吭哧跑来送",
+        "src" :"https://s26.postimg.cc/6ujnq82a1/anni00088.jpg"
+    },
+    {
+        "text": "谢谢猪头给我准备的生日礼物！难为你了，时差还没倒过来，还得想着各种准备😘虽然我知道你下午暗搓搓的一定在准备什么，但是真的没想到你会送我单反。刚进门的时候看到相机，我以为你是为了专门录下来==结果没想到。。😌",
+        "src": "https://s26.postimg.cc/apn1tdlm1/anni00089.jpg"
+    },
+    {
+        "text": "花也很好看，第一次收到花 嗯！还是懂我的审美的",
+        "src": "https://s26.postimg.cc/52qovbqmx/anni00090.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/i6w980y49/anni00091.jpg"
+    },
+    {
+        "text": "",
+        "src": "https://s26.postimg.cc/3np46mcp5/anni00092.jpg"
+    },
+    {
+        "text": "我帅吗？蓝色内裤露出来了嗷",
+        "src": "https://s26.postimg.cc/mg0za7gt5/anni00093.jpg"
+    },
+    {
+        "text": "想着纪念日还是走点心，反正也是写网页的，挺顺手的就做了起来。我想我们可以把这个一直做下去吧。爱你",
+        "src": "https://s26.postimg.cc/nv2jywuqx/anni00094.jpg"
+    }
+]
