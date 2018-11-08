e7db690b0f69966eb6f0cb2153cc94f1c0d34956
shiqihan <hsq5218@163.com>
2018-11-08 10:59:03 +0800

    finish array

--- js-assessment/app/arrays.js
+++ js-assessment/app/arrays.js
@@ -75,38 +75,124 @@ exports.arraysAnswers = {
   },
 
   truncate: function(arr) {
-
+    /* 修改原数组
+    arr.pop()
+    return arr; */
+    // 不修改原数组
+    // return arr.slice(0, arr.length-1);
+    var newArr = [];
+    for(var i=0,len=arr.length-1;i<len;i++){
+      newArr.push(arr[i])
+    }
+    return newArr;
   },
 
   prepend: function(arr, item) {
-
+    // arr.splice(0,0,item)
+    // return arr;
+    var newArr = [item];
+    for(var i=0,len=arr.length;i<len;i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   curtail: function(arr) {
-
+    // arr.shift();
+    // return arr;
+    var newArr = [];
+    for(var i=1,len=arr.length;i<len;i++){
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   concat: function(arr1, arr2) {
-
+      // return arr1.concat(arr2);
+    var newArr = [];
+    for(var i=0,len=arr1.length;i<len;i++){
+      newArr.push(arr1[i]);
+    }
+    for(var i=0,len=arr2.length;i<len;i++){
+      newArr.push(arr2[i]);
+    }
+    return newArr;
   },
 
   insert: function(arr, item, index) {
-
+    // arr.splice(index, 0, item);
+    // return arr;
+    var newArr = [];
+    for(var i=0,len=arr.length;i<len;i++){
+      if(i===index){
+        newArr.push(item);
+      }
+      newArr.push(arr[i]);
+    }
+    return newArr;
   },
 
   count: function(arr, item) {
-
+    return arr.filter(i => i===item).length;
+    // return arr.reduce((count, i) => {
+    //   return count+= (i===item ? 1 : 0) 
+    // }, 0)
+    // var count = 0;
+    // for(var i=0,len=arr.length;i<len;i++){
+    //   if(arr[i]===item){
+    //     count++;
+    //   }
+    // }
+    // return count;
   },
 
   duplicates: function(arr) {
-
+    // var hash = {},
+    //     result = [];
+    // for(var i=0,len=arr.length;i<len;i++){
+    //   var key = arr[i];
+    //   if(!hash[key]){
+    //     hash[key] = 1;
+    //   }else{
+    //     hash[key]++;
+    //     if(result.indexOf(key)===-1){
+    //       result.push(key)
+    //     }
+    //   }
+    // }
+    // return result;
+    // 排序
+    arr.sort();
+    var result = [];
+    for(var i=0,len=arr.length;i<len-1;i++){
+      if(arr[i]===arr[i+1]){
+        if(result.indexOf(arr[i])===-1){
+          result.push(arr[i]);
+        }
+      }
+    }
+    return result;
   },
 
   square: function(arr) {
-
+    // return arr.map(i=>{
+    //   return i*i;
+    // })
+    var newArr = [];
+    for(var i=0,len=arr.length;i<len;i++){
+      newArr.push(arr[i]*arr[i]);
+    }
+    return newArr;
   },
 
   findAllOccurrences: function(arr, target) {
-
+    var result = [];
+    arr.forEach((i,index)=>{
+      // if(i===target){
+      //   result.push(index);
+      // }
+      i!==target || result.push(index);
+    });
+    return result;
   }
 };
