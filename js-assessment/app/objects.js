ea0faf309556d159ba352d205b7497885048f199
shiqihan <hsq5218@163.com>
2018-11-12 13:41:47 +0800

    object

--- js-assessment/app/objects.js
+++ js-assessment/app/objects.js
@@ -2,14 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+  	return fn.bind(obj)();
+  	// fn.call(obj);
+  	// fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+  	var result = [];
+  	for(var key in obj){
+  		if(obj.hasOwnProperty(key)){
+  			result.push(`${key}: ${obj[key]}`);
+  		}
+  	}
+  	return result;
   }
 };
